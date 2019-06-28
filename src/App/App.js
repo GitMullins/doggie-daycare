b6f52e1ca7338fbc68f517bbec51093260ac20ec
David Mullins <Mullins6323@gmail.com>
2019-06-27 23:33:35 -0500

    moved App.js to Home.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,34 +1,79 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+import firebase from 'firebase/app';
 
-import DogPen from '../components/DogPen/DogPen';
-import myDogs from './dogs';
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
+import Home from '../components/Home/Home';
 
-import StaffRoom from '../components/StaffRoom/StaffRoom';
-import myEmployees from './employees';
+// import fbConnection from '../helpers/data/connection';
+
+// fbConnection();
+
+// import DogPen from '../components/DogPen/DogPen';
+// import myDogs from './dogs';
+
+// import StaffRoom from '../components/StaffRoom/StaffRoom';
+// import myEmployees from './employees';
+
+// class App extends React.Component {
+//   state = {
+//     dogs: [],
+//     employees: [],
+//   }
+
+//   componentDidMount() {
+//     this.setState({ dogs: myDogs });
+//     this.setState({ employees: myEmployees });
+//   }
+
+//   render() {
+//     const { dogs } = this.state;
+//     const { employees } = this.state;
+
+//     return (
+//       <div className="App">
+//         <div>Doggo Daycare</div>
+//         <DogPen dogs={dogs} />
+//         <StaffRoom employees={employees} />
+//       </div>
+//     );
+//   }
+// }
 
 class App extends React.Component {
   state = {
-    dogs: [],
-    employees: [],
+    authed: false,
   }
 
   componentDidMount() {
-    this.setState({ dogs: myDogs });
-    this.setState({ employees: myEmployees });
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
-    const { dogs } = this.state;
-    const { employees } = this.state;
-
+    const { authed } = this.state;
+    const loadComponent = () => {
+      if (authed) {
+        return (
+          <Home />
+        );
+      }
+      return <Auth />;
+    };
     return (
-      <div className="App">
-        <div>Doggie Daycare</div>
-        <DogPen dogs={dogs} />
-        <StaffRoom employees={employees} />
-      </div>
+    <div className="App">
+      <MyNavbar authed={authed} />
+      {loadComponent()}
+    </div>
     );
   }
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import DogPen from '../DogPen/DogPen';
+import myDogs from '../../App/dogs';
+
+import StaffRoom from '../StaffRoom/StaffRoom';
+import myEmployees from '../../App/employees';
+
+class Home extends React.Component {
+  state = {
+    dogs: [],
+    employees: [],
+  }
+
+  componentDidMount() {
+    this.setState({ dogs: myDogs });
+    this.setState({ employees: myEmployees });
+  }
+
+  render() {
+    const { dogs } = this.state;
+    const { employees } = this.state;
+
+    return (
+      <div className="Home">
+        <div>Doggo Daycare</div>
+        <DogPen dogs={dogs} />
+        <StaffRoom employees={employees} />
+      </div>
+    );
+  }
+}
+
+export default Home;
